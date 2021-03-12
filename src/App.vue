b97b7e6a8d6a8e0f3a4770e756ce1fe7dc0f2a61
figureair <48664874+figureair@users.noreply.github.com>
2021-03-12 14:25:16 +0800

    feat: add kg and tmp test

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <h1>知识图谱可视化系统</h1>
     <h4 id="tools">Echarts+Vue+SpringBoot+Neo4j</h4>
     <KG/>
+    <h6>KG666小组呈现</h6>
   </div>
 </template>
 
--- src/components/KG.vue
+++ src/components/KG.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-  <div id="indicator">
-  </div>
-  <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+  <div id="box">
+    <div id="myChart"></div>
+    <div id="text-box">
+      <select>
+      <option id="to1button" @click="to1">关系图</option>
+      <option id="to2button" @click="to2">力引导图</option>
+      <option id="to3button" @click="to3">环形关系图</option>
+      </select>
+    </div>
   </div>
 </template>
 
 
 <script>
+import $ from 'jquery'
+let myChart;
+let savedgraph;
+const ROOT_PATH = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
+
+//目前存有三种模式，后续迭代将加入更多表现模式
+//1.关系图
+let option1;
+//2.力引导图
+let option2;
+//3.环形关系图
+let option3;
+
 export default {
   name: "KG",
   mounted(){
@@ -15,34 +33,188 @@ export default {
   },
   methods: {
     drawLine(){
-      // 基于准备好的dom，初始化echarts实例
-      let myChart = this.$echarts.init(document.getElementById('myChart'))
+      // 初始化echarts实例
+      myChart = this.$echarts.init(document.getElementById('myChart'))
       // 绘制图表
-      myChart.setOption({
-        title: { text: '在Vue中使用echarts' },
-        tooltip: {},
-        xAxis: {
-          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
+
+      myChart.showLoading();
+      $.getJSON(ROOT_PATH + '/data/asset/data/les-miserables.json', function (graph) {
+        myChart.hideLoading();
+        //保存原始数据
+        savedgraph=JSON.parse(JSON.stringify(graph))
+
+        //初始设置为option1
+        graph.nodes.forEach(function (node) {
+          node.label = {
+            show: node.symbolSize > 30
+          };
+        });
+        option1 = {
+          tooltip: {},
+          legend: [{
+            // selectedMode: 'single',
+            data: graph.categories.map(function (a) {
+              return a.name;
+            })
+          }],
+          animationDuration: 1500,
+          animationEasingUpdate: 'quinticInOut',
+          series: [
+            {
+              name: 'Les Miserables',
+              type: 'graph',
+              layout: 'none',
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
+              roam: true,
+              label: {
+                position: 'right',
+                formatter: '{b}'
+              },
+              lineStyle: {
+                color: 'source',
+                curveness: 0.3
+              },
+              emphasis: {
+                focus: 'adjacency',
+                lineStyle: {
+                  width: 10
+                }
+              }
+            }
+          ]
+        };
+        myChart.setOption(option1);
+
+        //预存option2
+        graph=JSON.parse(JSON.stringify(savedgraph))
+        graph.nodes.forEach(function (node) {
+          node.symbolSize = 5;
+        });
+        option2 = {
+          title: {
+            text: 'Les Miserables',
+            subtext: 'Default layout',
+            top: 'bottom',
+            left: 'right'
+          },
+          tooltip: {},
+          legend: [{
+            // selectedMode: 'single',
+            data: graph.categories.map(function (a) {
+              return a.name;
+            })
+          }],
+          series: [
+            {
+              draggable: true,
+              name: 'Les Miserables',
+              type: 'graph',
+              layout: 'force',
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
+              roam: true,
+              label: {
+                position: 'right'
+              },
+              force: {
+                repulsion: 100
+              }
+            }
+          ]
+        };
+
+        //预存option3
+        graph=JSON.parse(JSON.stringify(savedgraph))
+        graph.nodes.forEach(function (node) {
+          node.label = {
+            show: node.symbolSize > 30
+          };
+        });
+        option3 = {
+          title: {
+            text: 'Les Miserables',
+            subtext: 'Circular layout',
+            top: 'bottom',
+            left: 'right'
+          },
+          tooltip: {},
+          legend: [{
+            data: graph.categories.map(function (a) {
+              return a.name;
+            })
+          }],
+          animationDurationUpdate: 1500,
+          animationEasingUpdate: 'quinticInOut',
+          series: [
+            {
+              name: 'Les Miserables',
+              type: 'graph',
+              layout: 'circular',
+              circular: {
+                rotateLabel: true
+              },
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
+              roam: true,
+              label: {
+                position: 'right',
+                formatter: '{b}'
+              },
+              lineStyle: {
+                color: 'source',
+                curveness: 0.3
+              },
+              emphasis: {
+                focus: 'adjacency',
+                lineStyle: {
+                  width: 10
+                }
+              }
+            }
+          ]
+        };
+
       });
-    }
+    },
+    to1(){
+      myChart.clear();
+      myChart.setOption(option1);
+    },
+    to2(){
+      myChart.clear();
+      myChart.setOption(option2);
+    },
+    to3(){
+      myChart.clear();
+      myChart.setOption(option3);
+    },
+
   }
 }
 </script>
 
 <style scoped>
-#indicator {
-  position: absolute;
-  left: 60px;
-  bottom: 20px;
-  text-align: left;
-  color: #f2f2f2;
-  font-size: 12px;
+#box{
+  display: flex;
+  width:100%;
+
+}
+#myChart{
+  width: 70%;
+  height: 500px;
+  border:1px solid rgba(200,200,200,0.75);
+  border-radius: 10px;
+  margin-left:30px;
+}
+#text-box{
+  width: 24%;
+  height: 500px;
+  border:1px solid rgba(200,200,200,0.75);
+  border-radius: 10px;
+  margin-left:20px;
 }
 </style>
