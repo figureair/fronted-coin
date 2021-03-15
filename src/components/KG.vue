8aea5bb96d9a40c5b873d7dde9ebe5ff8162ac62
figureair <48664874+figureair@users.noreply.github.com>
2021-03-16 00:41:26 +0800

    fix: move myChart to data

--- src/components/KG.vue
+++ src/components/KG.vue
@@ -131,7 +131,6 @@
 <script>
 import $ from 'jquery'
 
-let myChart;
 const ROOT_PATH = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/asset/data/les-miserables.json';
 
 //目前存有三种模式，后续迭代将加入更多表现模式
@@ -166,6 +165,7 @@ export default {
       addNodeVisible: false,
       addEdgeVisible: false,
       savedgraph:'',
+      myChart:'',
     }
   },
 
@@ -181,20 +181,20 @@ export default {
 
         let echarts = require('echarts');
 
-        myChart = echarts.init(document.getElementById('myChart'))
-        myChart.showLoading();
+        that.myChart = echarts.init(document.getElementById('myChart'))
+        that.myChart.showLoading();
         $.getJSON(ROOT_PATH, function (graph) {
-          myChart.hideLoading();
+          that.myChart.hideLoading();
           //保存原始数据
           that.savedgraph = JSON.parse(JSON.stringify(graph))
           that.initpage()
         });
 
 
-        myChart.on('click', 'series.graph', function (event) {
+        that.myChart.on('click', 'series.graph', function (event) {
           console.log(event);
           that.selectedItem.length = 0;
-          let opt = myChart.getOption();
+          let opt = that.myChart.getOption();
           let id = event.dataIndex;
           console.log(opt);
           switch (event.dataType) {
@@ -216,232 +216,219 @@ export default {
     },
 
     initpage() {
+        $("#selector").val('关系图');
 
-      $("#selector").val('关系图');
+        let that = this
+        //初始设置为option1
+        let graph = JSON.parse(JSON.stringify(that.savedgraph))
+        graph.nodes.forEach(function (node) {
+          node.label = {
+            show: node.symbolSize >= 30
+          };
+        });
+        graph.links.forEach(function (link) {
+          if (link.name === "dot") {
+            link.lineStyle = {type: 'dotted'}
+          }
+        });
+        option1 = {
+          tooltip: {
+            position: 'right',
+            extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'
+          },
+          //图例
+          legend: [{
+            data: graph.categories.map(function (a) {
+              return a.name;
+            })
+          }],
+          animationDuration: 1500,
+          animationEasingUpdate: 'quadraticIn',
+          series: [
+            {
+              type: 'graph',
+              //不采用任何布局
+              layout: 'none',
+              //关闭悬停图例高亮
+              legendHoverLink: false,
+              //节点大小不随鼠标缩放而缩放
+              nodeScaleRatio: 0,
+              //开启鼠标缩放和漫游
+              roam: true,
+              //边两端的标记
+              edgeSymbol: ['none', 'arrow'],
+              //边两端的标记大小
+              edgeSymbolSize: 5,
+              //悬停时鼠标样式
+              cursor: 'pointer',
 
-      let that=this
-      //初始设置为option1
-      let graph = JSON.parse(JSON.stringify(that.savedgraph))
-      graph.nodes.forEach(function (node) {
-        node.label = {
-          show: node.symbolSize >= 30
-        };
-      });
-      graph.links.forEach(function (link) {
-        if (link.name === "dot") {
-          link.lineStyle = {type: 'dotted'}
-        }
-      });
-      option1 = {
-        tooltip: {
-          position: 'right',
-          extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'
-        },
-        //图例
-        legend: [{
-          data: graph.categories.map(function (a) {
-            return a.name;
-          })
-        }],
-        animationDuration: 1500,
-        animationEasingUpdate: 'quadraticIn',
-        series: [
-          {
-            type: 'graph',
-            //不采用任何布局
-            layout: 'none',
-            //关闭悬停图例高亮
-            legendHoverLink: false,
-            //节点大小不随鼠标缩放而缩放
-            nodeScaleRatio: 0,
-            //开启鼠标缩放和漫游
-            roam: true,
-            //边两端的标记
-            edgeSymbol: ['none', 'arrow'],
-            //边两端的标记大小
-            edgeSymbolSize: 5,
-            //悬停时鼠标样式
-            cursor: 'pointer',
-
-            data: graph.nodes,
-            links: graph.links,
-            categories: graph.categories,
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
 
 
-            label: {
-              position: 'right',
-              formatter: '{b}'
-            },
+              label: {
+                position: 'right',
+                formatter: '{b}'
+              },
 
-            lineStyle: {
-              color: 'source',
-              curveness: 1,
-              width: 2,
-            },
-
-            emphasis: {
-              scale: true,
-              focus: 'adjacency',
               lineStyle: {
-                width: 10
+                color: 'source',
+                curveness: 1,
+                width: 2,
+              },
+
+              emphasis: {
+                scale: true,
+                focus: 'adjacency',
+                lineStyle: {
+                  width: 10
+                }
+              }
+            }
+          ],
+
+          toolbox: {
+            id: 'pie-chart',
+            itemSize: 15,
+            itemGap: 10,
+            right: 10,
+
+            feature: {
+              dataView: {
+                show: true,
+              },
+
+              restore: {
+                show: true,
               }
             }
           }
-        ],
-
-        toolbox: {
-          id: 'pie-chart',
-          itemSize: 15,
-          itemGap: 10,
-          right: 10,
-
-          feature: {
-            dataView: {
-              show: true,
-            },
-
-            restore: {
-              show: true,
-            }
-          }
-        }
-      };
-      myChart.setOption(option1);
-
-
-      //预存option2
-      graph = JSON.parse(JSON.stringify(that.savedgraph))
-      graph.nodes.forEach(function (node) {
-        node.label = {
-          show: node.symbolSize >= 30
         };
-      });
-      graph.links.forEach(function (link) {
-        if (link.name === "dot") {
-          link.lineStyle = {type: 'dotted', width: '2'}
-        }
-      });
-      option2 = {
-        tooltip: {
-          position: 'right',
-          extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'
-        },
-        //图例
-        legend: [{
-          data: graph.categories.map(function (a) {
-            return a.name;
-          })
-        }],
-        series: [
-          {
-            //当前视角的缩放比例
-            zoom: 2,
-            //是否可拖动
-            draggable: true,
-            type: 'graph',
-            //类型为力引导图
-            layout: 'force',
+        that.myChart.setOption(option1);
 
-            data: graph.nodes,
-            links: graph.links,
-            categories: graph.categories,
 
-            //开启鼠标缩放和漫游
-            roam: true,
-            label: {
-              position: 'right'
-            },
-            force: {
-              //斥力
-              repulsion: 100
-            }
+        //预存option2
+        graph = JSON.parse(JSON.stringify(that.savedgraph))
+        graph.nodes.forEach(function (node) {
+          node.label = {
+            show: node.symbolSize >= 30
+          };
+        });
+        graph.links.forEach(function (link) {
+          if (link.name === "dot") {
+            link.lineStyle = {type: 'dotted', width: '2'}
           }
-        ]
-      };
+        });
+        option2 = {
+          tooltip: {
+            position: 'right',
+            extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'
+          },
+          //图例
+          legend: [{
+            data: graph.categories.map(function (a) {
+              return a.name;
+            })
+          }],
+          series: [
+            {
+              //当前视角的缩放比例
+              zoom: 2,
+              //是否可拖动
+              draggable: true,
+              type: 'graph',
+              //类型为力引导图
+              layout: 'force',
 
-      //预存option3
-      graph = JSON.parse(JSON.stringify(that.savedgraph))
-      graph.nodes.forEach(function (node) {
-        node.label = {
-          show: node.symbolSize >= 30
-        };
-      });
-      graph.links.forEach(function (link) {
-        if (link.name === "dot") {
-          link.lineStyle = {type: 'dotted', width: '2'}
-        }
-      });
-      option3 = {
-        tooltip: {
-          position: 'right',
-          extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'
-        },
-        //图例
-        legend: [{
-          data: graph.categories.map(function (a) {
-            return a.name;
-          })
-        }],
-        animationDurationUpdate: 1500,
-        animationEasingUpdate: 'quinticInOut',
-        series: [
-          {
-            type: 'graph',
-            layout: 'circular',
-            circular: {
-              rotateLabel: true
-            },
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
 
-            data: graph.nodes,
-            links: graph.links,
-            categories: graph.categories,
-
-            roam: true,
-
-            label: {
-              position: 'right',
-              formatter: '{b}'
-            },
-            lineStyle: {
-              color: 'source',
-              curveness: 0.3
-            },
-            emphasis: {
-              focus: 'adjacency',
-              lineStyle: {
-                width: 10
+              //开启鼠标缩放和漫游
+              roam: true,
+              label: {
+                position: 'right'
+              },
+              force: {
+                //斥力
+                repulsion: 100
               }
             }
+          ]
+        };
+
+        //预存option3
+        graph = JSON.parse(JSON.stringify(that.savedgraph))
+        graph.nodes.forEach(function (node) {
+          node.label = {
+            show: node.symbolSize >= 30
+          };
+        });
+        graph.links.forEach(function (link) {
+          if (link.name === "dot") {
+            link.lineStyle = {type: 'dotted', width: '2'}
           }
-        ]
-      };
+        });
+        option3 = {
+          tooltip: {
+            position: 'right',
+            extraCssText: 'box-shadow: 0 0 3px rgba(0, 0, 0, 0.3)'
+          },
+          //图例
+          legend: [{
+            data: graph.categories.map(function (a) {
+              return a.name;
+            })
+          }],
+          animationDurationUpdate: 1500,
+          animationEasingUpdate: 'quinticInOut',
+          series: [
+            {
+              type: 'graph',
+              layout: 'circular',
+              circular: {
+                rotateLabel: true
+              },
 
-      console.log(option1.toString());
+              data: graph.nodes,
+              links: graph.links,
+              categories: graph.categories,
 
-      myChart.on('click', 'series.graph', function (e) {
-        console.log(e);
-        // let targetId = e.data.id;
+              roam: true,
 
-        let opt = myChart.getOption();
-
-        console.log(opt);
-
-
-      });
+              label: {
+                position: 'right',
+                formatter: '{b}'
+              },
+              lineStyle: {
+                color: 'source',
+                curveness: 0.3
+              },
+              emphasis: {
+                focus: 'adjacency',
+                lineStyle: {
+                  width: 10
+                }
+              }
+            }
+          ]
+        };
     },
 
     changeTo(value) {
+      let that=this
       $(document).ready(function () {
-        myChart.clear();
+        that.myChart.clear();
         switch (value) {
           case 1:
-            myChart.setOption(option1);
+            that.myChart.setOption(option1);
             break
           case 2:
-            myChart.setOption(option2);
+            that.myChart.setOption(option2);
             break
           case 3:
-            myChart.setOption(option3);
+            that.myChart.setOption(option3);
             break
         }
       })
@@ -523,9 +510,9 @@ export default {
     },
 
     downloadImg() {
-      $(document).ready(function () {
+
         let img = new Image();
-        img.src = myChart.getDataURL({
+        img.src = this.myChart.getDataURL({
           type: 'png',
           pixelRatio: 2,
           excludeComponents: ['toolbox']
@@ -535,7 +522,7 @@ export default {
         a.download = "knowledge-graph.png";
         a.href = img.src;
         a.dispatchEvent(e);
-      })
+
     },
 
     downloadJson() {
