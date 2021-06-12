0c58eab5d38e48a9af0e817d8cf5ebac21c23a5b
181840090 <181840090@smail.nju.edu.cn>
2021-06-12 13:46:02 +0800

    feat: Add 404 page.

---
+++ src/components/NetworkError.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NetworkError",
+}
+</script>
+
+<style scoped>
+#logo{
+  background: url("../img/02.jpeg");
+  background-size: 100% 100%;
+  position: absolute;
+  min-height: 600px;
+  height: 100%;
+  width: 100%
+}
+
+</style>
